19a10403731463f7c070c6d2c96186b8650d20a5
ruabbit <34975428+ruaabbit@users.noreply.github.com>
2024-02-08 23:18:21 +0800

    Update diy-part2.sh

--- scripts/zn-m2/diy-part2.sh
+++ scripts/zn-m2/diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 #优先安装 passwall 源
-./scripts/feeds install -a -f -p passwall_packages
-./scripts/feeds install -a -f -p passwall_luci
+#./scripts/feeds install -a -f -p passwall_packages
+#./scripts/feeds install -a -f -p passwall_luci
